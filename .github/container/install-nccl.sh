ceb953fb289a86da19c261bb85e61873936ae6ea
Shawn Wang <shawnw@nvidia.com>
2024-02-29 19:01:25 -0800

    Fixes for shawnw
    always install latest nccl version
    
    upgrade nccl
    
    use debug build
    
    fix typo

--- .github/container/install-nccl.sh
+++ .github/container/install-nccl.sh
@@ -5,33 +5,60 @@ set -ex -o pipefail
 export DEBIAN_FRONTEND=noninteractive
 export TZ=America/Los_Angeles
 
-# If NCCL is already installed, don't reinstall it. Print a message and exit
-if dpkg -s libnccl2 libnccl-dev &> /dev/null; then
-    echo "NCCL is already installed. Skipping installation."
-else
-    apt-get update
 
-    # Extract CUDA version from `nvcc --version` output line
-    # Input: "Cuda compilation tools, release X.Y, VX.Y.Z"
-    # Output: X.Y
-    cuda_version=$(nvcc --version | sed -n 's/^.*release \([0-9]*\.[0-9]*\).*$/\1/p')
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
 
-    # Find latest NCCL version compatible with existing CUDA by matching
-    # ${cuda_version} in the package version string
-    libnccl2_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
-    libnccl_dev_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
-    if [[ -z "${libnccl2_version}" || -z "${libnccl_dev_version}" ]]; then
-        echo "Could not find compatible NCCL version for CUDA ${cuda_version}"
-        exit 1
-    fi
+apt-get update
 
-    apt-get install -y \
-        libnccl2=${libnccl2_version} \
-        libnccl-dev=${libnccl_dev_version}
+# Extract CUDA version from `nvcc --version` output line
+# Input: "Cuda compilation tools, release X.Y, VX.Y.Z"
+# Output: X.Y
+cuda_version=$(nvcc --version | sed -n 's/^.*release \([0-9]*\.[0-9]*\).*$/\1/p')
 
-    apt-get clean
-    rm -rf /var/lib/apt/lists/*
+# Find latest NCCL version compatible with existing CUDA by matching
+# ${cuda_version} in the package version string
+libnccl2_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
+libnccl_dev_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
+if [[ -z "${libnccl2_version}" || -z "${libnccl_dev_version}" ]]; then
+    echo "Could not find compatible NCCL version for CUDA ${cuda_version}"
+    exit 1
 fi
+
+apt-get install -y \
+    libnccl2=${libnccl2_version} \
+    libnccl-dev=${libnccl_dev_version}
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+# If NCCL is already installed, don't reinstall it. Print a message and exit
+# if dpkg -s libnccl2 libnccl-dev &> /dev/null; then
+#     echo "NCCL is already installed. Skipping installation."
+# else
+#    apt-get update
+#
+#    # Extract CUDA version from `nvcc --version` output line
+#    # Input: "Cuda compilation tools, release X.Y, VX.Y.Z"
+#    # Output: X.Y
+#    cuda_version=$(nvcc --version | sed -n 's/^.*release \([0-9]*\.[0-9]*\).*$/\1/p')
+#
+#    # Find latest NCCL version compatible with existing CUDA by matching
+#    # ${cuda_version} in the package version string
+#    libnccl2_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
+#    libnccl_dev_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
+#    if [[ -z "${libnccl2_version}" || -z "${libnccl_dev_version}" ]]; then
+#        echo "Could not find compatible NCCL version for CUDA ${cuda_version}"
+#        exit 1
+#    fi
+#
+#    apt-get install -y \
+#        libnccl2=${libnccl2_version} \
+#        libnccl-dev=${libnccl_dev_version}
+#
+#    apt-get clean
+#    rm -rf /var/lib/apt/lists/*
+# fi
 
 # Create a prefix with include/ and lib/ directories containing symlinks to the NCCL
 # version installed at the system level; this is useful to pass to XLA to avoid it
